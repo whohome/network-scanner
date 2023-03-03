df8980e20800091e7cd100e4f23a91071cca6ed6
jesse <92591280+jessejes@users.noreply.github.com>
2023-03-03 09:39:54 +0100

    Create api route for local network scanner

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
+from fastapi import FastAPI
+from scanner import scan_mac_addresses
+from models import Target
 
+app = FastAPI()
+
+
+@app.get("/")
+def get_mac_address(target: Target):
+    return scan_mac_addresses(target_ip=target.target)
