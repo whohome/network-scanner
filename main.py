76394b4520de70419eb40baac0dc73e5c8bf98c8
jesse <92591280+jessejes@users.noreply.github.com>
2023-04-16 08:59:33 +0200

    Scanner repariert

--- main.py
+++
@@ -1,9 +0,0 @@
-from fastapi import FastAPI
-from scanner import scan_mac_addresses
-
-app = FastAPI()
-
-
-@app.get("/")
-def scan():
-    return scan_mac_addresses(target_ip="192.168.178.255")
--- scanner.py
+++ scanner.py
@@ -1,23 +1,46 @@
 from scapy.all import srp
 from scapy.layers.l2 import ARP, Ether
+from socket import gethostname, gethostbyname, gethostbyaddr
+
+# Holen Sie den Hostnamen
+hostname = gethostname()
+
+# Holen Sie die IP-Adresse des Hostnamens
+ip_address = gethostbyname(hostname)
+
+# Holen Sie die Netzwerkadresse aus der IP-Adresse und der Netzwerkmaske
+net_address = '.'.join(ip_address.split('.')[:3]) + '.0'
+
+# Definieren Sie die Ziel-IP-Adresse
+target_ip = f"{net_address}/24"
+
+# Erstellen Sie ARP-Paket, um MAC-Adresse abzurufen
+arp = ARP(pdst=target_ip)
+ether = Ether(dst="ff:ff:ff:ff:ff:ff")
+packet = ether/arp
+
+# Senden Sie das Paket und erhalten Sie die Antwort
+result = srp(packet, timeout=3, verbose=0)[0]
+
+# Analyse der Antwort
+devices = []
+for sent, received in result:
+    devices.append({'ip': received.psrc, 'mac': received.hwsrc})
+
+print(devices)
+
+# Überprüfen Sie, ob die gesuchte MAC-Adresse im Netzwerk vorhanden ist
+target_mac = "36:39:A4:10:37:12"
+found = False
+for device in devices:
+
+    if device['mac'] == target_mac.lower():
+        found = True
+
+if found:
+    print(f"Das Gerät mit der MAC-Adresse {target_mac} wurde im Netzwerk gefunden.")
+else:
+    print(f"Das Gerät mit der MAC-Adresse {target_mac} konnte nicht im Netzwerk gefunden werden.")
 
 
-def scan_mac_addresses(ip: str):
-    arp = ARP(pdst=ip)
 
-    ether = Ether(dst="ff:ff:ff:ff:ff:ff")
-
-    packet = arp / ether
-
-    result = srp(packet, timeout=3, verbose=0)[0]
-
-    clients = []
-
-    for sent, received in result:
-        clients.append({'ip': received.src, 'mac': received.hwsrc})
-
-    return clients
-
-
-if __name__ == '__main__':
-    print(scan_mac_addresses("192.168.178.255"))
